8c39186293ca9aeecf8d7f8a2057d5616ca314de
Yuta0227 <yutahonjo@keio.jp>
2023-09-21 23:30:48 +0900

    エラー対処

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -28,18 +28,18 @@ return [
             //     'path' => '/images/icons/icon-144x144.png',
             //     'purpose' => 'any'
             // ],
-            '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
-                'purpose' => 'any'
-            ],
-            '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
-                'purpose' => 'any'
-            ],
-            '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
-                'purpose' => 'any'
-            ],
+            // '152x152' => [
+            //     'path' => '/images/icons/icon-152x152.png',
+            //     'purpose' => 'any'
+            // ],
+            // '192x192' => [
+            //     'path' => '/images/icons/icon-192x192.png',
+            //     'purpose' => 'any'
+            // ],
+            // '384x384' => [
+            //     'path' => '/images/icons/icon-384x384.png',
+            //     'purpose' => 'any'
+            // ],
             // '512x512' => [
             //     'path' => '/images/icons/icon-512x512.png',
             //     'purpose' => 'any'
