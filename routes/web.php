57420d5f619e70d9139a32d8234c732ca2e434ea
Yuta0227 <yutahonjo@keio.jp>
2023-09-21 21:23:21 +0900

    表示確認

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('/test','TestController');
+Route::resource('/test',TestController::class);
