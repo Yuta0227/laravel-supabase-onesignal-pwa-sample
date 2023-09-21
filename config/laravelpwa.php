4e5cc08ba5371418be1eed6125384692377101df
Yuta0227 <yutahonjo@keio.jp>
2023-09-22 01:11:51 +0900

    iconsは空ではいけないらしい

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -12,10 +12,10 @@ return [
         'orientation'=> 'any',
         'status_bar'=> 'black',
         'icons' => [
-            // '72x72' => [
-            //     'path' => '/images/icons/icon-72x72.png',
-            //     'purpose' => 'any'
-            // ],
+            '72x72' => [
+                'path' => '/images/icons/icon-72x72.png',
+                'purpose' => 'any'
+            ],
             // '96x96' => [
             //     'path' => '/images/icons/icon-96x96.png',
             //     'purpose' => 'any'
