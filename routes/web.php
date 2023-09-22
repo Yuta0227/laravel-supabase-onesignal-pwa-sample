cba37afc0d59def56ded06bf85ca4f71da601c02
Yuta0227 <yutahonjo@keio.jp>
2023-09-22 17:14:16 +0900

    robots.txtは表示されるが、画像とserviceworker.jsが表示されない。storagelinkをすることで画像は出るのでは

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Artisan::call('storage:link');
     return view('welcome');
 });
 Route::resource('/test',TestController::class);
