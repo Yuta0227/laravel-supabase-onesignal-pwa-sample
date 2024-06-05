c32decef745a5d61c583eb58349feaecf7dee890
Yuta Honjo <yuta0227.msc28@gmail.com>
2024-06-06 00:39:50 +0900

    php artisan serveとnpm run devでhttpsで画面表示できるようになった。mysqlサーバーは自分で建てる必要がある。

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+const host="localhost";
+const port=8000;
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+            // refresh: [...refreshPaths, "app/Livewire/**"],
         }),
+        basicSsl(),
     ],
+    server: {
+        https: true,
+        proxy: {
+            "^(?!(/@vite|/resources|/node_modules))": {
+                target: `http://${host}:${port}`,
+            },
+        },
+        host,
+        port: 5173,
+        hmr: { host },
+    },
 });
