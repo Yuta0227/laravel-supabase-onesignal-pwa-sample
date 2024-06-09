44744c621182a2f740cbea329234f6486e04edfa
Yuta Honjo <yuta0227.msc28@gmail.com>
2024-06-09 18:21:46 +0900

    デプロイできていた時に戻す

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,19 @@
 import { defineConfig } from "vite";
-import laravel, { refreshPaths } from "laravel-vite-plugin";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-
-const host = "localhost";
-const port = 8000;
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
-            buildDirectory: "/public/build/assets/",
         }),
-        // basicSsl(),
     ],
-    server: {
-        https: true,
-        proxy: {
-            "^(?!(/@vite|/resources|/node_modules))": {
-                target: `http://${host}:${port}`,
-            },
-        },
-        host,
-        port: 5173,
-        hmr: { host },
-    },
     build: {
-        manifest: true,
-        outDir: "public/build",
-        rollupOptions: {
-            output: {
-                entryFileNames: "assets/[name]-[hash].js",
-                chunkFileNames: "assets/[name]-[hash].js",
-                assetFileNames: "assets/[name]-[hash].[ext]",
-            },
+        /** If you set esmExternals to true, this plugins assumes that 
+          all external dependencies are ES modules */
+
+        commonjsOptions: {
+            esmExternals: true,
         },
     },
 });
