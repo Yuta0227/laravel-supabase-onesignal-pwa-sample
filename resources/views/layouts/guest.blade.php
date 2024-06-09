9bdd5f510ac01ccb39d795aa39b027e014eaaa71
Yuta Honjo <yuta0227.msc28@gmail.com>
2024-06-09 13:04:26 +0900

    パス変更

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,8 +13,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    <link href="/build/assets/app.css" rel="stylesheet">
-    <script src="/build/assets/app.js"></script>
+    <link href="/resources/css/app.css" rel="stylesheet">
+    <script src="/resources/js/app.js"></script>
     <script>
         console.log('yes')
     </script>
